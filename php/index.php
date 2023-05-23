a4d01c13a87451f230865704b049d799425567fd
b5463 <64685803+b5463@users.noreply.github.com>
2023-05-23 17:23:28 +0200

    id; private func

--- php/index.php
+++ php/index.php
@@ -11,6 +11,18 @@ class Logger
     public static $studentLogFile = "studenti.json";
     public static $arrivalLogFile = "prichody.json";
 
+    private static function getNextId(string $filename): int
+    {
+        if (file_exists($filename)) {
+            $data = json_decode(file_get_contents($filename), true);
+            if (is_array($data)) {
+                $ids = array_column($data, 'id');
+                return max($ids) + 1;
+            }
+        }
+        return 1;
+    }
+
     public static function getCurrentDateTime()
     {
         return date("Y-m-d H:i:s");
@@ -34,7 +46,6 @@ class Logger
 
     public static function processStudentArrival(string $studentArrivalTime, string $studentName = '')
     {
-
         if ($studentArrivalTime > "08:00:00") {
             $logData = self::getCurrentDateTime() . " - meskanie";
         } elseif ($studentArrivalTime >= "20:00:00" && $studentArrivalTime <= "23:59:59") {
@@ -53,10 +64,6 @@ class Logger
 
     public static function saveStudent(string $studentName)
     {
-        $student = [
-            'name' => $studentName
-        ];
-
         $studentLogFile = self::$studentLogFile;
         $arrivalLogFile = self::$arrivalLogFile;
         $students = [];
@@ -70,11 +77,18 @@ class Logger
             $arrivals = json_decode(file_get_contents($arrivalLogFile), true);
         }
 
+        $studentId = self::getNextId($studentLogFile);
+        $student = [
+            'id' => $studentId,
+            'name' => $studentName
+        ];
         $students[] = $student;
         $encodedStudents = json_encode($students, JSON_PRETTY_PRINT);
         file_put_contents($studentLogFile, $encodedStudents);
 
+        $arrivalId = self::getNextId($arrivalLogFile);
         $arrival = [
+            'id' => $arrivalId,
             'arrival_time' => self::getCurrentDateTime()
         ];
         $arrivals[] = $arrival;
@@ -111,6 +125,7 @@ class Logger
 
         if (!empty($students) && !empty($arrivals)) {
             foreach ($students as $index => $student) {
+                echo "ID žiaka: " . $student['id'] . "<br>";
                 echo "Meno žiaka: " . $student['name'] . "<br>";
                 echo "Čas príchodu: " . $arrivals[$index]['arrival_time'] . "<br><br>";
             }
@@ -128,6 +143,7 @@ class Logger
             foreach ($arrivals as $index => $arrival) {
                 if (strtotime($arrival['arrival_time']) > strtotime("08:00:00")) {
                     $studentName = $students[$index]['name'] ?? "Unknown";
+                    echo "ID žiaka: " . $students[$index]['id'] . "<br>";
                     echo "Meno žiaka: " . $studentName . " - Neskorý príchod<br>";
                     echo "Čas príchodu: " . $arrival['arrival_time'] . "<br><br>";
                 }
@@ -147,8 +163,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 
-if (isset($_GET['meno'])) {
-    $studentName = $_GET['meno'];
+if (isset($_GET['name'])) {
+    $studentName = $_GET['name'];
     Logger::processStudentArrival(Logger::getCurrentDateTime(), $studentName);
 }
 
